a4270882afdec796e2609a93beab775ccc3de748
CarlZhong2014 <CarlZhong2014@gmial.com>
2014-11-29 15:25:01 +0800

    add commit in tsUserv.py

--- exercise/Network_program/SimpleUDPSC/tsUserv.py
+++ exercise/Network_program/SimpleUDPSC/tsUserv.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-  
 from socket import *
 from time import ctime
 
 HOST = ''
-PORT = 8080
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
+PORT = 8080		#设置端口号
+BUFSIZ = 1024		#设置缓冲大小为1k
+ADDR = (HOST, PORT)	#通信地址
 
 udpSocket = socket(AF_INET, SOCK_DGRAM)
 udpSocket.bind(ADDR)
