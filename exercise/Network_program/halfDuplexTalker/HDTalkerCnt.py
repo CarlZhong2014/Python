55fdabf69a6904382a727529f78d860234056b1b
CarlZhong2014 <CarlZhong2014@gmail.com>
2014-12-12 08:59:41 +0800

    HPTalker are complete

--- exercise/Network_program/halfDuplexTalker/HDTalkerCnt.py
+++ exercise/Network_program/halfDuplexTalker/HDTalkerCnt.py
@@ -1,22 +1,49 @@
-# -*- coding: utf-8 -*- 
-from socket import *
-HOST = 'localhost'
-PORT = 3300				#设置端口号
+#coding=utf-8
+import socket
+HOST = 'localhost'			#设置要链接的主机的IP
+PORT = 3300					#设置要链接的主机端口号
 BUFSIZ = 1024				#设置缓冲区大小，设定为1K
 ADDR = (HOST, PORT)	
+tryCon = 0
 
-tcpCliSock = socket(AF_INET, SOCK_STREAM)
-tcpCliSock.connect(ADDR)	#与服务端链接
+def TCnt():
+	tcpCliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	while True:
+		try:
+			tcpCliSock.connect(ADDR)	#与服务端链接
+		except:
+			print u"正在尝试连接远程主机 "
+			tryCon+=1		
+			if tryCon == 3:
+				print u"无法连接上远程主机，请稍后再试"
+				exit()
+		else:
+			break
+	print u'登陆成功（通讯结束请输入"quit"退出）\n'
 
-while True:
-	data = raw_input('>')	#输入要发送的数据
-	if not data:			#判断数据是否为空
-		break
-	tcpCliSock.send(data)	#发送数据
-	print u'Waitting...\n'
-	data = tcpCliSock.recv(BUFSIZ)	#接收服务器返回的数据
-	if not data:
-		break
-	print data 				#输出服务器返回的数据
+	try:
+		while True:
+			data = raw_input('I:>  ')
+			if not data:			
+				continue
+			elif data == 'quit':		#通知服务端工作完成
+				tcpCliSock.send(data)	
+				break
+			else:
+				tcpCliSock.send(data)	
 
-tcpCliSock.close()
+			while True:
+				data = tcpCliSock.recv(BUFSIZ)	
+
+				if not data:
+					continue
+				else:
+					print data 	
+					break			
+	except socket.error, e:
+		print "Session closing"
+		print e
+	tcpCliSock.close()
+
+if __name__=="__main__":
+	TCnt()
--- exercise/Network_program/halfDuplexTalker/HDTalkerSer.py
+++ exercise/Network_program/halfDuplexTalker/HDTalkerSer.py
@@ -1,31 +1,52 @@
-#coding=utf-8
-from socket import *
-from time import ctime
+#coding=utf-8				
+import socket               	#加载socket模块
+from time import ctime			#加载time模块ctime类
 
-HOST = ''
-PORT = 3300
-BUSIZ = 1024
-ADDR = (HOST, PORT)
+HOST = ''						#设置主机IP，为空则表示本机上所有的网卡ip。
+PORT = 3300						#设置端口号
+BUSIZ = 1024					#设置数据缓冲区，1KB
+ADDR = (HOST, PORT)				
 
-TSerSock = socket(AF_INET, SOCK_STREAM)
-TSerSock.bind(ADDR)
-TSerSock.listen(1)
-
-while True:
-	print u'Waitting for connection...'
-	TCntSock, cntAddr = TSerSock.accept()
-	print u'...connection from:', cntAddr
-	rData = TCntSock.recv(BUSIZ)
-	if not rData:
-		TCnSock.close()
-		break
-
-	print u'From [%s] %s \n%s' % (cntAddr[0], ctime(), rData)
-	
-	sData = raw_input(u'>')
-	TCntSock.send(u'From [%s] %s \n%s' %
-		(cntAddr[0], ctime(), sData))
+def closeTCnt():				#
 	TCntSock.close()
+	print "Session closing.."
+	
+TSerSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#创建一个TCP sokcet
+TSerSock.bind(ADDR)				#绑定TCP socket的地址和端口
+TSerSock.listen(1)				#只接收一个请求，如后续后其他请求则会被拒绝
+try:
+	while True:
+		print 'Waitting for connection...'
+		(TCntSock, cntAddr) = TSerSock.accept()		#TSerSock.accept 返回一个(conn, addr)的元组。 conn是一个socket对象，而addr是客户端IP地址和端口
+		print '...connection from:', cntAddr
+		
+		try: 
 
-TSerSock.close()
+			while True:
+				rData = TCntSock.recv(BUSIZ)		#接收客户端信息
+				if not rData:
+					continue
+				elif rData == 'quit':				#当收到‘quit’后关闭TCntSock并等待下一次链接
+					break
+				else:
+					print 'From [%s] %s \n  %s' % (cntAddr[0], ctime(), rData)	
+					
+			
+				while True:
+					sData = raw_input('I:>  ')		
+					if not sData:
+						continue
+					else:	
+						TCntSock.send('From [%s] %s \n  %s' %
+							(cntAddr[0], ctime(), sData))		#发送服务器信息。
+						break
+		
+		except 	socket.error ,detail:			#发生错误是关闭TCntSock,输出错误信息并等待下一次链接
+			print detail
+		closeTCnt()
+
+finally:
+	TSerSock.close()
+
+
 
